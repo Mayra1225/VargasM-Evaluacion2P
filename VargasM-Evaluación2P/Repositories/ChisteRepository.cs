b52c9a863cc46206527b17ce4bacdeae981330b0
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-06-02 07:25:36 -0500

    Creacion del Repositorio para el consumo de Apis

---
+++ VargasM-Evaluación2P/Repositories/ChisteRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Metal;
+
+namespace VargasM_Evaluación2P.Repositories
+{
+    public class ChisteRepository
+    {
+        private readonly HttpClient _httpClient;
+
+
+        public ChisteRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Chiste> GetChisteRandom()
+        {
+            try
+            {
+                var url = "https://official-joke-api.appspot.com/random_joke";
+                var response = await _httpClient.GetStringAsync(url);
+                var chiste = System.Text.Json.JsonSerializer.Deserialize<Chiste>(response);
+                return chiste;
+            }
+            catch (Exception ex)
+            {
+                return new Chiste
+                {
+                    setup = "Error al obtener el chiste",
+                    punchline = ex.Message
+                };
+            }
+        }
+        public class Chiste
+        {
+            public string setup { get; set; }
+            public string punchline { get; set; }
+        }
+    }
+}
