006118bbcd1cd0148b6a2a865d2a390fb9bc7c04
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-06-02 07:16:57 -0500

    Configuracion de la Main Page

---
+++ VargasM-Evaluación2P/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace VargasM_Evaluación2P
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnChisteClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ChistesPage());
+        }
+
+        private async void OnAboutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+
+    }
